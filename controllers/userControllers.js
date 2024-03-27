80333cf97ca423133c2bd66d5ec32753e0f6eb9b
Katherine Yanan Saung <katysaung@gmail.com>
2024-03-27 19:41:24 -0400

    created and tested in postman all routes and controllers for user

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,7 +1,7 @@
 import expressAsyncHandler from "express-async-handler";
+export { registerUser, login, logout, updateUser, deleteUser };
 
-// Register User
-// route    POST /api/users/register
+// route    POST /api/user
 // access   Public
 const registerUser = expressAsyncHandler(async (req, res) => {
   // res.status(401);
@@ -10,4 +10,26 @@ const registerUser = expressAsyncHandler(async (req, res) => {
   res.status(200).json({ message: "Registered User" });
 });
 
-export { registerUser };
+// route  POST /api/user/auth
+// JWT    Private
+const login = expressAsyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Authorized User" });
+});
+
+// route  POST /api/user/logout
+// JWT    Private
+const logout = expressAsyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Logged Out User" });
+});
+
+// route  PUT /api/user/profile
+// JWT    Private
+const updateUser = expressAsyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Updated User" });
+});
+
+// route  DELETE /api/user/profile
+// JWT    Private
+const deleteUser = expressAsyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Deleted User" });
+});
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,17 @@
 import express from "express";
 const router = express.Router();
-import { registerUser } from "../controllers/userControllers.js";
+import {
+  registerUser,
+  login,
+  logout,
+  updateUser,
+  deleteUser,
+} from "../controllers/userControllers.js";
 
 router.post("/", registerUser);
+router.post("/auth", login);
+router.post("/logout", logout);
+router.put("/profile", updateUser);
+router.delete("/profile", deleteUser);
 
 export default router;
